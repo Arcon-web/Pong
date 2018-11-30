e8ab5bd34a83c85d7155baeaec359b166754eef8
TheMats5 <32159057+TheMats5@users.noreply.github.com>
2018-11-30 11:38:43 +0100

    paddle movement

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ function startGame() {
 var ball;
 var paddle1;
 var paddle2;
+var upKey;
+var downKey;
+var velocity = 5;
+
 
 function preload(){
     game.load.image('paddle', 'assets/paddle.png');
@@ -15,15 +19,26 @@ function preload(){
 
 function create()
 {
+    //controls for paddle
+    upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+        
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = "#000";
+
+    //creation of paddles
     paddle1 = create_paddle(0,game.world.centerY);
-    paddle2 = create_paddle(game.world.width-16,game.world.centerY);
+    paddle2 = create_paddle(game.world.width-8,game.world.centerY);
+    paddle1.scale.setTo(0.5);
+    paddle2.scale.setTo(0.5);
+
 
 }
 
 function update()
 {
+    control_paddle(paddle1,game.input.y);
+
 
 }
 
@@ -34,5 +49,16 @@ function create_paddle(x,y)
     game.physics.arcade.enable(paddle);
     paddle.body.collideWorldBounds = true;
     return paddle;
+}
+function control_paddle(paddle,y)
+{
+    
+    if(upKey.isDown)
+    {
+        paddle.y-=velocity;
+    }else if(downKey.isDown)
+    {
+        paddle.y+=velocity;
+    }
 }
 
