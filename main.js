3a9928ac9708bf810449e5e172334437f12b198f
TheMats5 <32159057+TheMats5@users.noreply.github.com>
2018-11-30 10:41:57 +0100

    added paddles and background

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 var game;
 
 function startGame() {
-	game = new Phaser.Game(320, 480, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+	game = new Phaser.Game(800, 680, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 	console.log("Game created!");
 }
 
 var ball;
+var paddle1;
+var paddle2;
 
 function preload(){
-    
+    game.load.image('paddle', 'assets/paddle.png');
 }
 
-function create() {
-
+function create()
+{
     game.physics.startSystem(Phaser.Physics.ARCADE);
-  
-    
-    
-
-}
-function update(){
+    game.stage.backgroundColor = "#000";
+    paddle1 = create_paddle(0,game.world.centerY);
+    paddle2 = create_paddle(game.world.width-16,game.world.centerY);
 
 }
 
+function update()
+{
+
+}
+
+function create_paddle(x,y)
+{
+    var paddle = game.add.sprite(x,y,'paddle')
+    paddle.anchor.setTo(0.5,0.5);
+    game.physics.arcade.enable(paddle);
+    paddle.body.collideWorldBounds = true;
+    return paddle;
+}
 
