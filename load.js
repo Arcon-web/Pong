b15c5cbdae1f4511d3264fb329e2b8d13a59a477
TheMats5 <32159057+TheMats5@users.noreply.github.com>
2018-12-02 18:12:21 +0100

    added crossorigin

--- load.js
+++ load.js
@@ -3,7 +3,7 @@ let loadState = {
     {
 
         let loadingLabel = game.add.text(80, 150, 'loading...',{font: '30px Courier', fill: '#ffffff'});
-        
+        this.load.crossOrigin = "Anonymous";
         game.load.image('paddle', 'assets/paddle.png');
         game.load.image('ball', 'assets/ball.png');
 
